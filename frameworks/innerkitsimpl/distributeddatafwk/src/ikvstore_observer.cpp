e3885f657f3752e29a294854281895cf899b4e51
zuojiangjiang <zuojiangjiang@huawei.com>
2022-03-25 21:55:04 +0800

    fix error Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- frameworks/innerkitsimpl/distributeddatafwk/src/ikvstore_observer.cpp
+++ frameworks/innerkitsimpl/distributeddatafwk/src/ikvstore_observer.cpp
@@ -98,7 +98,7 @@ void KvStoreObserverProxy::OnChange(const ChangeNotification &changeNotification
         ZLOGE("Write ChangeNotification buffer size to parcel failed.");
         return;
     }
-    ZLOGD("I(%" PRId64") U(%" PRId64") D(%" PRId64") T(%" PRId64")",
+    ZLOGD("I %" PRId64" U %" PRId64" D %" PRId64" T %" PRId64"",
           insertBufferSize, updateBufferSize, deleteBufferSize, totalBufferSize);
     if (totalBufferSize < Constant::SWITCH_RAW_DATA_SIZE) {
         if (!data.WriteParcelable(&changeNotification)) {
