998f5b0de6528e8d5125892fd4ecd5f48a6bee94
wangbin <wangbin341@huawei.com>
2022-04-26 18:30:29 +0800

    Signed-off-by: wangbin <wangbin341@huawei.com>
     Changes to be committed:

---
+++ services/distributeddataservice/libs/distributeddb/test/fuzztest/common/distributeddb_tools_test.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DISTRIBUTEDDB_TOOLS_TEST_H
+#define DISTRIBUTEDDB_TOOLS_TEST_H
+
+#include <mutex>
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+
+#include "db_types.h"
+#include "kv_store_changed_data.h"
+#include "kv_store_delegate_impl.h"
+#include "kv_store_delegate_manager.h"
+#include "kv_store_nb_delegate.h"
+#include "kv_store_observer.h"
+#include "log_print.h"
+#include "message.h"
+#include "query.h"
+
+namespace DistributedDBTest {
+    struct DatabaseInfo {
+        std::string appId{};
+        std::string userId{};
+        std::string storeId{};
+        std::string dir{};
+        int dbUserVersion = 0;
+    };
+
+
+    class DistributedDBToolsTest final {
+    public:
+        DistributedDBToolsTest() {}
+
+        ~DistributedDBToolsTest() {}
+
+        DistributedDBToolsTest(const DistributedDBToolsTest &) = delete;
+
+        DistributedDBToolsTest &operator=(const DistributedDBToolsTest &) = delete;
+
+        DistributedDBToolsTest(DistributedDBToolsTest &&) = delete;
+
+        DistributedDBToolsTest &operator=(DistributedDBToolsTest &&) = delete;
+
+      
+        static void TestDirInit(std::string &);
+
+        // remove the test db files in the test directory of dir.
+        static int RemoveTestDbFiles(const std::string &);
+
+        static int GetCurrentDir(std::string& dir);
+
+    };
+} // namespace DistributedDBTest
+#endif // DISTRIBUTEDDB_TOOLS_TEST_H
