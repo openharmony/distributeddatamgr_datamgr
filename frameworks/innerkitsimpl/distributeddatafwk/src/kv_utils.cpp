a5e536c5eb48a800f4c02bdc8447fb3a1df8801c
anan <anyueling@huawei.com>
2022-05-27 15:48:42 +0800

    update
    Signed-off-by: anan <anyueling@huawei.com>

--- frameworks/innerkitsimpl/distributeddatafwk/src/kv_utils.cpp
+++ frameworks/innerkitsimpl/distributeddatafwk/src/kv_utils.cpp
@@ -15,12 +15,12 @@
 
 #define LOG_TAG "KvUtils"
 
+#include "kv_utils.h"
 #include <endian.h>
 #include "cov_util.h"
 #include "log_print.h"
 #include "data_query.h"
 #include "kvstore_datashare_bridge.h"
-#include "kv_utils.h"
 
 namespace OHOS {
 namespace DistributedKv {
